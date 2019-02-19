092558334448b6952b6a4d2cd7061bad25c5dc49
ku4eto1 <koceto_kb1@abv.bg>
2019-02-19 16:26:48 +0200

    AASDF213

--- Test1.java
+++ Test1.java
@@ -3,5 +3,6 @@ class Test1{
     public static void main(String [] args){
         System.out.println("raga");
         System.out.println("Zdrasti");
+        System.out.println("Zdrasti23");
     }
 }
