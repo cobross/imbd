f3df79b8719dd7d064e75ab8fc6e490c596eb6e3
koceto <koceto_kb1@abv.bg>
2019-02-19 16:24:46 +0200

    IC-2018.3.3 <up313@WINSUP Update Test1.java	Delete ui.lnf.xml

--- Test1.java
+++ Test1.java
@@ -1,6 +1,7 @@
 import java.util.*;
 class Test1{
     public static void main(String [] args){
-    System.out.println("raga");
+        System.out.println("raga");
+        System.out.println("Zdrasti");
     }
 }
